b7aacc0a1a7f70812831af013eac3afa459e7722
Chris Foster <chriswf@Chriss-MacBook-Pro.local>
2017-12-06 10:03:53 -0600

    some notes added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 angular
   .module("myApp", ["ui.router"])
-  .config(function($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) { // this has to be a standard es5 function. 
+    //stateProvider state references a view. stateProvider configures multiple different views. 
+    // and urlRouterProvider provides a singular service request called otherwise. If can't find what you're looking for,
+    // you land back on ''/'' 
     $stateProvider
       .state("home", {
         url: "/",
-        templateUrl: "js/home/homeTmpl.html"
+        templateUrl: "js/home/homeTmpl.html" //can put a div in `back ticks`, like template: `<div> Home </div>
+        // can solo add controller: 'whateverCtrl' here. This attaches the controller to the state you view here.
       })
       .state("products", {
         url: "/products/:id",
