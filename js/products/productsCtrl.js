47ebfbf95f53904a3176374d52066b2daa7ac973
Chris Foster <chriswfoster@gmail.com>
2017-12-06 10:42:11 -0600

    fixed some comments

--- js/products/productsCtrl.js
+++ js/products/productsCtrl.js
@@ -1,5 +1,6 @@
 angular.module('myApp').controller('productsCtrl', function( $scope, $stateParams, productsSrvc ) {
     
+
       if ( $stateParams.id === 'shoes' ) {
         productsSrvc.getShoeData().then( function( response ) { // productServc gomes from productsSrvc.js, where it runs
           // a get similar to axios get.
@@ -8,6 +9,7 @@ angular.module('myApp').controller('productsCtrl', function( $scope, $stateParam
       } else if ( $stateParams.id === 'socks' ) {
         productsSrvc.getSockData().then( function( response ) {
           $scope.productData = response.data;
+          // all of this can be handledin Service if you want to the data to be accessed by other controllers
         });
       }
     
--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,16 +1,37 @@
-angular.module('myApp').service('productsSrvc', function( $http ) { //called with a new keyword, returns an object.
+angular.module("myApp").service("productsSrvc", function($http) {
+  //called with a new keyword, returns an object.
   // we brought $http in so we can use $http to make the requests, just as axios makes requests
-    this.getShoeData = function() {
-      return $http({
-        method: 'GET',
-        url: 'https://practiceapi.devmountain.com/products?category=shoes'
-      });
-    };
-  
-    this.getSockData = function() {
-      return $http({
-        method: 'GET',
-        url: 'https://practiceapi.devmountain.com/products?category=socks'
-      });
-    };
-  });
+
+  // var socks = []; SEE LAST COMMENTS FOR THIS EXPLANATION
+
+  this.getShoeData = function() {
+    return $http({
+      method: "GET",
+      url: "https://practiceapi.devmountain.com/products?category=shoes"
+    })
+  }
+
+  this.getSockData = function() {
+    return $http({
+      method: "GET",
+      url: "https://practiceapi.devmountain.com/products?category=socks"
+    })
+  }
+})
+
+//     CAN ALSO DO:
+// this.getSocksInfo = function(){
+//   return $http
+//   .get("https://swapi.co/api/people")
+//   .then (response => {
+//     console.log(response)
+//     socks = response.data.results
+//     //return response.data.results
+//   })
+// }
+// but they have productData being mapped out with ng-repeat in productsTmpl.html, sent out from ONLY productsCtrl. Having that variable stored only here makes it global.
+
+// looks like there's multiple ways to do this
+
+// can also do:
+// $http.post(base_url, {})
