79c11f9081cebac7e9a186cd1db9a1a17bb00297
Chris Foster <chriswfoster@gmail.com>
2017-12-06 11:53:53 -0600

    yet more comments and notes

--- js/products/productsCtrl.js
+++ js/products/productsCtrl.js
@@ -1,16 +1,19 @@
-angular.module('myApp').controller('productsCtrl', function( $scope, $stateParams, productsSrvc ) {
-    
+angular
+  .module("myApp")
+  .controller("productsCtrl", function($scope, $stateParams, productsSrvc) {
+    // can test this with $scope.test = productSrvc.test
+    //then put {{ test }} on your index.html, after adding script tags for productSrvc and ~Ctrl  in bottom of index.html
 
-      if ( $stateParams.id === 'shoes' ) {
-        productsSrvc.getShoeData().then( function( response ) { // productServc gomes from productsSrvc.js, where it runs
-          // a get similar to axios get.
-          $scope.productData = response.data;
-        });
-      } else if ( $stateParams.id === 'socks' ) {
-        productsSrvc.getSockData().then( function( response ) {
-          $scope.productData = response.data;
-          // all of this can be handledin Service if you want to the data to be accessed by other controllers
-        });
-      }
-    
-    });
+    if ($stateParams.id === "shoes") {
+      productsSrvc.getShoeData().then(function(response) {
+        // productServc gomes from productsSrvc.js, where it runs
+        // a get similar to axios get.
+        $scope.productData = response.data
+      })
+    } else if ($stateParams.id === "socks") {
+      productsSrvc.getSockData().then(function(response) {
+        $scope.productData = response.data
+        // all of this can be handledin Service if you want to the data to be accessed by other controllers
+      })
+    }
+  })
